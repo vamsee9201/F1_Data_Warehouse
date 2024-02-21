2ee8d8d59890beb436bed63adf274a77d408d488
vamsee9201 <vamseekrishna9201@gmail.com>
2024-02-21 17:01:11 -0600

    queried a public dataset on bq

--- loadDrivers.py
+++ loadDrivers.py
@@ -1,5 +1,11 @@
+#%%
 from google.cloud import bigquery
-client = bigquery.Client()
+from google.oauth2 import service_account
+
+credentialsFileName = "vamseeSAcredentials.json"
+credentials = service_account.Credentials.from_service_account_file(
+    credentialsFileName)
+client = bigquery.Client(credentials=credentials)
 QUERY = (
     'SELECT name FROM `bigquery-public-data.usa_names.usa_1910_2013` '
     'WHERE state = "TX" '
@@ -9,3 +15,4 @@ rows = query_job.result()  # Waits for query to finish
 
 for row in rows:
     print(row.name)
+# %%
