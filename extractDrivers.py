e275d798ad2446910ed66bc1dcbdb9682071a868
vamsee9201 <vamseekrishna9201@gmail.com>
2024-02-21 14:29:38 -0600

    parsed access key and secret key

--- extractDrivers.py
+++ extractDrivers.py
@@ -1,3 +1,9 @@
-import boto3
-import pandas
 #%%
+import boto3
+import pandas as pd
+#%%
+credentials = pd.read_csv("aws_credentials.csv")
+# %%
+accessKeyId = credentials['Access key ID'].iloc[0]
+scretAccessKey = credentials['Secret access key'].iloc[0]
+# %%
