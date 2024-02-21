d3c8a6b5f614e13ea8f4c46a78498563d1d28e53
vamsee9201 <vamseekrishna9201@gmail.com>
2024-02-21 15:49:59 -0600

    created drivers extract file

--- extractDrivers.py
+++ extractDrivers.py
@@ -1,23 +1,17 @@
-#%%
 import boto3
 import pandas as pd
 import requests
 import json
-#%%
-credentials = pd.read_csv("aws_credentials.csv")
-# %%
-accessKeyId = credentials['Access key ID'].iloc[0]
-scretAccessKey = credentials['Secret access key'].iloc[0]
-# %%
 url = "https://5oo1xx09xb.execute-api.us-east-1.amazonaws.com/default/teams"
 headers = {
     "Content-Type": "application/json"
 }
 data = {
-    "year": 2023
-}
-response = requests.put(url, headers=headers, data=json.dumps(data))
-# %%
-driversJson = response.json()
-driversDf = pd.DataFrame(driversJson)
-# %%
+         "year": 2014
+    }
+def extractDriversData(year):
+    data['year'] = year
+    response = requests.put(url, headers=headers, data=json.dumps(data))
+    driversJson = response.json()
+    driversDf = pd.DataFrame(driversJson)
+    return driversDf
