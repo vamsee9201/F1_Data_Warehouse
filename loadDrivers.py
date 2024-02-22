959e8dc09659f361d1123dca884ff76e315d1ad6
vamsee9201 <vamseekrishna9201@gmail.com>
2024-02-21 19:28:09 -0600

    successfully tested data appending into big query

--- loadDrivers.py
+++ loadDrivers.py
@@ -1,18 +1,39 @@
 #%%
 from google.cloud import bigquery
 from google.oauth2 import service_account
+import typing
+
+if typing.TYPE_CHECKING:
+    from google.cloud import bigquery
+import datetime
+import pytz
+import pandas as pd
+#%%
 
 credentialsFileName = "vamseeSAcredentials.json"
 credentials = service_account.Credentials.from_service_account_file(
     credentialsFileName)
 client = bigquery.Client(credentials=credentials)
-QUERY = (
-    'SELECT name FROM `bigquery-public-data.usa_names.usa_1910_2013` '
-    'WHERE state = "TX" '
-    'LIMIT 100')
-query_job = client.query(QUERY)  # API request
-rows = query_job.result()  # Waits for query to finish
+#%%
+tableId = "f1datawarehouse.f1Test.driversData"
+jobConfig = bigquery.LoadJobConfig(
+    write_disposition = "WRITE_APPEND",
+)
+def loadData(df):
+    job = client.load_table_from_dataframe(
+        df,tableId,jobConfig
+    )
+    job.result()
+    return "loaded data"
 
-for row in rows:
-    print(row.name)
+# %%
+import extractDrivers
+import transformDrivers
+
+if __name__ == "__main__":
+    json = extractDrivers.extractDriversData(2022)
+    print(json)
+    df = transformDrivers.transformDrivers(json)
+    print(df)
+    loadData(df)
 # %%
