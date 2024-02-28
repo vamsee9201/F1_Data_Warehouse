5f45093867c126716f88cde76ea2d2a9444e1b71
vamsee9201 <vamseekrishna9201@gmail.com>
2024-02-27 18:30:26 -0600

    added extract teams function as well.

--- dags/extract.py
+++ dags/extract.py
@@ -14,6 +14,7 @@ def extractDriversData(year):
     data['year'] = year
     print("printing data >>> ",data)
     print("getting response >>>")
+    url = "https://5oo1xx09xb.execute-api.us-east-1.amazonaws.com/default/drivers"
     try :
         response = requests.put(url, headers=headers, data=json.dumps(data), timeout=30)
     except Exception as e :
@@ -22,3 +23,20 @@ def extractDriversData(year):
     driversJson = response.json()
     print("returning JSON data >>>")
     return driversJson
+
+def extractTeamsData(year):
+    print("this is the extract >>>")
+    print("year >>>", year)
+    data['year'] = year
+    print("printing data >>> ",data)
+    print("getting response >>>")
+    url = "https://5oo1xx09xb.execute-api.us-east-1.amazonaws.com/default/teams"
+    try :
+        response = requests.put(url, headers=headers, data=json.dumps(data), timeout=30)
+    except Exception as e :
+        print("exception created >>>>", e)
+    print("getting driversJson data >>>")
+    teamsJson = response.json()
+    print("returning JSON data >>>")
+    return teamsJson
+    
